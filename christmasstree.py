8707aed805e5a653a991ffbc4f5b27c5fb604afc
vdermati <vdermati@snf-687781.(none)>
2016-01-14 16:53:26 +0200

    christmasstree

---
+++ christmasstree.py
@@ -0,0 +1,16 @@
+n=6
+z=n-1
+x=1 
+i=0
+for i in range(1,5):
+ print(' '*z+ '*'*x +' '*z)
+ z=z-1
+ i=i+1 
+ x=x+2
+for i in range(5,6) :
+     print('     *      ')
+     print('     *      ')
+ 
+ 
+ 
+ 
